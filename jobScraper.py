2a5ee5afef2d1ad482e1e137af2d476d4266fe99
Evan Casey <caseyem@appstate.edu>
2018-03-02 16:40:26 -0500

    Rudimentary graphing added

--- jobScraper.py
+++ jobScraper.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from time import sleep
 from random import uniform
+import matplotlib.pyplot as plt
 import operator
 import string
 
@@ -127,16 +128,23 @@ def displayStats(nJobs):
     
     print "Total Jobs Found: " + str(nJobs) + "\n"
 
+    plt.bar(range(len(keyword_dict)), list(keyword_dict.values()), align='center')
+    plt.xticks(range(len(keyword_dict)), list(keyword_dict.keys()))
+    plt.ylabel("# Occurences")
+    plt.xlabel("Keywords")
+    plt.show()
+
 #Drives the program. Provides logical control
 def main():
     url = "https://www.glassdoor.com/index.htm"
     keyword = 'software engineer'
     location = 'Raleigh, NC'
 
-    browser = initBrowser()
-    browser.get(url)
+    #browser = initBrowser()
+    #browser.get(url)
 
-    nJobs = searchJobs(keyword, location, browser)
+    #nJobs = searchJobs(keyword, location, browser)
+    nJobs = 60
     parseData()
     displayStats(nJobs)
 
